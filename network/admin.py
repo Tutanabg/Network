1f8ad5ecd2d4c5a2590ce4b142804b00b7713f93
Tutanabg <66039997+Tutanabg@users.noreply.github.com>
2020-12-21 18:44:55 -0500

    Update admin.py

--- network/admin.py
+++ network/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import User, post, profile
+# Register your models here.
+admin.site.register(User) 
+admin.site.register(post) 
+admin.site.register(profile) 
+
 
