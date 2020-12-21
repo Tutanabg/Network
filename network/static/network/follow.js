cf72095eeaa3f5f5b2377a1a422046fd52f75791
Tutanabg <66039997+Tutanabg@users.noreply.github.com>
2020-12-21 17:52:29 -0500

    Update follow.js

--- network/static/network/follow.js
+++ network/static/network/follow.js
@@ -1,1 +1,25 @@
+document.addEventListener('DOMContentLoaded', 
+function () {
+	mb = document.querySelector("#users_followed");
+	follow_btn = document.querySelector("#follow-btn");
+	follow_btn.addEventListener("click", (e) => {
+	foll = follow_btn.getAttribute("data-user");
+	cont = follow_btn.textContent;
+   fetch('/follow_user', {
+      method: 'POST',
+      body: JSON.stringify({
+      "foll": foll,
+      "cont": cont,
+})
+    })
+    .then(res => res.json())
+    .then(res => {
+     });
+   follow_btn.textContent="Unfollow";
+     
+     });
+     });
+    
+     
+   
 
