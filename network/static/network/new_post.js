d3dfea1ec23abcf732a2eb8f861e5dc5fef52e12
Tutanabg <66039997+Tutanabg@users.noreply.github.com>
2020-12-21 17:55:01 -0500

    Update new_post.js

--- network/static/network/new_post.js
+++ network/static/network/new_post.js
@@ -1,1 +1,25 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const myform = document.querySelector("#new_form");
+    myform.addEventListener("submit", (event) => {
+      event.preventDefault();
+    const post = document.querySelector('#fill_post').value;
+  fetch('/new_post', {
+      method: 'POST',
+      body: JSON.stringify({
+        "post": post,
+      }),
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        alert("post added");
+         });      
+         });
+});
+
+
+
+
 
